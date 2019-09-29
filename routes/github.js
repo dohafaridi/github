17db238857d38188ceafff9836cace04a30cca64
Doha Faridi <dohafaridi@gmail.com>
2019-09-29 22:55:17 +0200

    Caching with redis

--- routes/github.js
+++ routes/github.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const axios = require("axios");
+const redis = require("redis");
 
+const client = redis.createClient();
 const router = express.Router();
+
+const setResponse = (res, user, repos) =>
+  res.render("pages/home", { user, repos });
+
+const cacheMiddleware = (req, res, next) =>
+  client.get(req.body.user, (_, data) => {
+    if (data) {
+      return setResponse(res, req.body.user, JSON.parse(data));
+    }
+    next();
+  });
 
 router.get("/", (_, res) => res.render("pages/home"));
 
-router.post("/users", ({ body: { user } }, res) =>
+router.post("/users", cacheMiddleware, ({ body: { user } }, res) =>
   axios
     .get(`https://api.github.com/users/${user}/repos`)
-    .then(({ data }) => res.render("pages/home", { user, repos: data }))
+    .then(({ data }) => {
+      client.setex(user, 3600, JSON.stringify(data));
+      return setResponse(res, user, data);
+    })
     .catch(err => res.render("pages/home", { user }))
 );
 
